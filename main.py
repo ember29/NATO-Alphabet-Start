5d38414d259cf8bc8891aa5bfa15b8cef60bc2ec
ember <vinayakdurgulevd@gmail.com>
2025-01-06 11:52:41 +0530

    python code file added.

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+
+data=pandas.read_csv("nato_phonetic_alphabet.csv")
+dic={row.letter:row.code for (index,row) in data.iterrows()}
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word=input("Enter a word:").upper()
+output=[dic[letter]for letter in word]
+print(output)
